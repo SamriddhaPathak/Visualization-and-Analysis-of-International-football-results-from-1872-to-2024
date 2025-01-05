cff40b6f418449f2e145ec8a42c3ee7ba35ad72d
Samriddha Pathak <samriddhapathak123333@gmail.com>
2025-01-05 23:11:10 +0545

    Update countries hosting most football matches.py

--- Analysis of Countries hosting most football matches/countries hosting most football matches.py
+++ Analysis of Countries hosting most football matches/countries hosting most football matches.py
@@ -1,7 +1,7 @@
 import plotly.express as px
 import pandas as pd
 from collections import Counter
-
+ 
 data = pd.read_csv("Data(file_format_CSV)/results.csv")
 
 data_country =  data['country'] 
@@ -16,11 +16,11 @@ no_of_hostings = [item[1] for item in top4_count_noof_hostings_by_countries]
 
 dft = {   
     'countries': countries,   
-    'no_of_hostings': no_of_hostings  
+    'no_of_hostings': no_of_hostings      
 }
 
 df = pd.DataFrame(dft)
-    
+      
 fig = px.bar(df, x = 'countries', y = 'no_of_hostings', color = 'countries', title = "Countries Hosting The Most Number of Football Mathces from 1872 to 2024")
 
 fig.show()    
